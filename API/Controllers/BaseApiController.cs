936128b40df2ccb574494d5b079516af7051ae25
Adrian De Simone <adesimone@mpublico.gov.ar>
2023-01-04 09:38:47 -0300

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // localhost:5000/api/*
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
